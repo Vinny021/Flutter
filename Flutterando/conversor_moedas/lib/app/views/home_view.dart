03ad7fdb4b33ad57f7950c883c52169b472fc6c1
Vinny021 <viniciusbarbosa021@gmail.com>
2021-07-14 11:47:16 -0300

    Alinhando Layout :art:

--- Flutterando/conversor_moedas/lib/app/views/home_view.dart
+++ Flutterando/conversor_moedas/lib/app/views/home_view.dart
@@ -8,27 +8,68 @@ class HomeView extends StatelessWidget {
       body: SizedBox(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        child: Column(
-          children: [
-            Center(
-              child: Container(
-                height: 100,
-                width: 100,
-                child: Image.asset('assets/images/currency.png'),
+        child: Padding(
+          padding: const EdgeInsets.only(top:100, left:25, right:25, bottom: 20),
+          child: Column(
+            children: [
+              Center(
+                child: Container(
+                  height: 150,
+                  width: 150,
+                  child: Image.asset('assets/images/currency.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Row(
-                children: [
-                  Expanded(child: DropdownButton(items: [], onChanged: (value) {},)),
-                  Expanded(child: TextField())
-                ],
+              SizedBox(height: 10),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: SizedBox(
+                        height:64,
+                        child: DropdownButton(
+                          underline: Container(
+                            height: 1,
+                            color: Colors.amber,
+                          ),
+                          isExpanded: true,
+                          items: [
+                            DropdownMenuItem(child: Text('Real')),
+                            DropdownMenuItem(child: Text('Dolar')),
+                          ], 
+                          onChanged: (value) {},
+                        ),
+                      )
+                    ),
+                    SizedBox(width: 15,),
+                    Expanded(
+                      flex: 2,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.amber)
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.amber)
+                          )
+                        ),
+                      )
+                    )
+                  ],
+                ),
               ),
-            ),
-            ElevatedButton(onPressed: () {}, child: Text('Converter'))
-              
-          ]
+              SizedBox(height:40),
+              ElevatedButton(
+                onPressed: () {}, 
+                child: Text('Converter'),
+                style: ButtonStyle(
+                  backgroundColor:MaterialStateProperty.all(Colors.amber), 
+                ),
+              )
+                
+            ]
+          ),
         ),
       ),
     );
