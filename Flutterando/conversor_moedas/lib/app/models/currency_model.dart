3abe9d6b991f418fa60bbe8b56dd35788a7c7093
Vinny021 <viniciusbarbosa021@gmail.com>
2021-07-14 00:52:39 -0300

    Currency Model

---
+++ Flutterando/conversor_moedas/lib/app/models/currency_model.dart
@@ -0,0 +1,26 @@
+class CurrencyModel {
+  const CurrencyModel({this.name, this.real, this.dolar, this.euro, this.bitcoin});
+
+  final String name;
+  final double real;
+  final double dolar;
+  final double euro;
+  final double bitcoin;
+
+  static List<CurrencyModel> getCurrencys() {
+    return <CurrencyModel>[
+      CurrencyModel(
+          name: 'Real', real: 1.0, dolar: 0.19, euro: 0.16, bitcoin: 0.0000059),
+      CurrencyModel(
+          name: 'Dolar', real: 5.16, dolar: 1, euro: 0.85, bitcoin: 0.000031),
+      CurrencyModel(
+          name: 'Euro', real: 6.09, dolar: 1.18, euro: 1, bitcoin: 0.000037),
+      CurrencyModel(
+          name: 'Bitcoin',
+          real: 164404.98,
+          dolar: 31838.60,
+          euro: 27017.35,
+          bitcoin: 1)
+    ];
+  }
+}
